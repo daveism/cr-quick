d80100eaed1b9e0f740ec17f541e590df56a566b
Dave Michelson <daveism@gmail.com>
2016-01-18 22:14:42 -0500

    initial commit

---
+++ js/map.js
@@ -0,0 +1,117 @@
+var tmsFolder = '../curtms/';
+var dataFolder = 'https://raw.githubusercontent.com/daveism/daveisms-assets/master/';
+
+var map = L.tileLayer('http://api.tiles.mapbox.com/v3/daveism.oo0p88l4/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  maxZoom: 15
+})
+
+
+var image = L.tileLayer('http://api.tiles.mapbox.com/v3/daveism.oo0o5k97/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  maxZoom: 15
+})
+
+
+var ndvi = L.tileLayer(tmsFolder + 'ndvi_tms/{z}/{x}/{y}.png', {
+  attribution: 'CR image <a href="http://www.landsatfact.com">LSF</a>',
+  tms:true,
+  maxZoom: 15,
+  opacity: 0.65
+})
+
+
+var ndmi = L.tileLayer(tmsFolder + 'ndmi_tms/{z}/{x}/{y}.png', {
+  attribution: 'CR image <a href="http://www.landsatfact.com">LSF</a>',
+  tms:true,
+  maxZoom: 15,
+  opacity: 0.65
+})
+
+var swir = L.tileLayer(tmsFolder + 'swir_tms/{z}/{x}/{y}.png', {
+  attribution: 'CR image <a href="http://www.landsatfact.com">LSF</a>',
+  tms:true,
+  maxZoom: 15,
+  opacity: 0.65
+});
+
+var baseMaps = {
+    "map": map,
+    "sat": image
+};
+
+var map = L.map('map',{
+  center: [36.730,-81.859],
+  zoom: 13,
+  layers: [map,image]
+});
+
+var overlayMaps = {
+    "NDVI": ndvi,
+    "NDMI": ndmi,
+    "SWIR": swir
+};
+
+var ctrl =  L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+//map.fitBounds(overlayMaps);
+
+$( "input[type=checkbox]" ).click(function( event ) {
+    layerClicked = window[event.target.value];
+    //alert(JSON.stringify(layerClicked));
+    // map.removeLayer(ndvi);
+    // map.removeLayer(ndmi);
+    // map.removeLayer(swir);
+    if (map.hasLayer(layerClicked)) {
+       map.removeLayer(layerClicked);
+    }
+    else{
+
+       map.addLayer(layerClicked);
+    } ;
+ });
+
+ $( ".btn-group [type=button]" ).click(function( event ) {
+    map.removeLayer(image);
+   if(this.value == 'sat'){
+         map.addLayer(image);
+         map.removeLayer(map);
+   }
+   if(this.value == 'map'){
+         map.addLayer(map);
+         map.removeLayer(image);
+   }
+  });
+
+  var changeCount = 0
+  var curpoint = 0
+  var geoJSON = '';
+
+$.getJSON(dataFolder + "swir_areas.geojson", function(response) {
+    console.log("response", response);
+    var geojsonLayer = new L.GeoJSON(response);
+    geojsonLayer.addTo(map);
+    geoJSON = response;
+    changeCount = geoJSON.features.length;
+});
+
+var getNextChange = function(){
+  if(curpoint=>changeCount){
+    curpoint++;
+    $("#currval").html('Change Feaute:' + curpoint)
+    //var t = geoJSON.features[curpoint]
+    var centroidPt = turf.centroid(geoJSON.features[curpoint]);
+    console.log(JSON.stringify(centroidPt))
+    map.setView([ centroidPt.geometry.coordinates[1] , centroidPt.geometry.coordinates[0]],15);
+  }
+}
+
+var getPevChange = function(){
+  if(curpoint >=  0){
+    curpoint--;
+    $("#currval").html('Change Feauture:&nbsp;&nbsp;' + curpoint)
+    var centroidPt = turf.centroid(geoJSON.features[curpoint]);
+    console.log(JSON.stringify(centroidPt))
+    map.setView([ centroidPt.geometry.coordinates[1] , centroidPt.geometry.coordinates[0]],15);
+  }
+}
---
+++ processdata.sh
@@ -0,0 +1,64 @@
+
+#!/bin/bash
+
+tms='../curtms'
+folder='../Downloads/lsfdata4/products'
+data='./data'
+processfolder='./processingdata'
+
+rm $data/*.tif
+rm $data/*.geojson
+rm $processfolder/*.tif
+rm $processfolder/*.geojson
+
+#clouds
+gdal_merge.py -n 0 -o $processfolder/cloud_all.tif $folder/cloud_mask/*.tif
+
+#ndvi
+gdal_merge.py -n 0 -o $processfolder/ndvi_all.tif $folder/ndvi/*.tif
+gdal_calc.py -A $processfolder/ndvi_all.tif -B $processfolder/cloud_all.tif  --A_band=1 --B_band=1 --outfile=$processfolder/ndvi_noclouds_all.tif --calc="A*B"
+gdaldem color-relief -of GTiff $processfolder/ndvi_noclouds_all.tif $folder/ndvicolor.txt $processfolder/ndvi_color.tif
+
+#ndvi
+gdal_merge.py -n 0 -o $processfolder/ndmi_all.tif $folder/ndmi/*.tif
+gdal_calc.py -A $processfolder/ndmi_all.tif -B $processfolder/cloud_all.tif  --A_band=1 --B_band=1 --outfile=$processfolder/ndmi_noclouds_all.tif --calc="A*B"
+gdaldem color-relief -of GTiff $processfolder/ndmi_noclouds_all.tif  $folder/ndmicolor.txt $processfolder/ndmi_color.tif
+
+#swir
+gdal_merge.py -n 0 -o $processfolder/swir_all.tif $folder/swir/*.tif
+gdal_calc.py -A $processfolder/swir_all.tif -B $processfolder/cloud_all.tif  --A_band=1 --B_band=1 --outfile=$processfolder/swir_noclouds_all.tif --calc="A*B"
+gdaldem color-relief -of GTiff $processfolder/swir_noclouds_all.tif  $folder//swircolor.txt $processfolder/swir_color.tif
+
+gdal_calc.py -A $processfolder/swir_noclouds_all.tif  --A_band=1  --outfile=$processfolder/swir_big.tif --calc="255*(A>168)" --NoDataValue=0
+gdal_polygonize.py $processfolder/swir_big.tif -f 'GeoJSON' $processfolder/swir_areas_raw.geojson
+ogr2ogr -f 'GeoJSON' $data/swir_areas.geojson $processfolder/swir_areas_raw.geojson  -t_srs EPSG:4326
+
+#create local TMS tile map server
+#does not really need a server
+gdal2tiles.py -w 'all' -z 5-9 -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 5-9 -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 5-9 -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
+
+gdal2tiles.py -w 'all' -z 10  -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 10  -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 10  -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
+
+gdal2tiles.py -w 'all' -z 11  -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 11  -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 11  -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
+
+gdal2tiles.py -w 'all' -z 12  -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 12  -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 12  -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
+
+gdal2tiles.py -w 'all' -z 13  -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 13  -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 13  -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
+
+gdal2tiles.py -w 'all' -z 14  -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 14  -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 14  -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
+
+gdal2tiles.py -w 'all' -z 15  -a 255 $processfolder/ndvi_color.tif $tms/ndvi_tms/ &
+gdal2tiles.py -w 'all' -z 15  -a 255 $processfolder/ndmi_color.tif $tms/ndmi_tms/ &
+gdal2tiles.py -w 'all' -z 15  -a 255 $processfolder/swir_color.tif $tms/swir_tms/ &
